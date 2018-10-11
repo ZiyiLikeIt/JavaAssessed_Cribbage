9d141f5434d4ccacbc9ba6b0aaa7e3677a08eb2d
ziyi <ziyi.github@outlook.com>
2018-10-12 10:22:00 +1100

    Initial Commit
    Create the project and classes

---
+++ src/HandValue.java
@@ -0,0 +1,25 @@
+/*****************************************
+ * 
+ * @filename HandValue.java
+ * 
+ * @project JavaAssessed_Cribbage
+ * 
+ * @brief the Java project for COMP90041
+ * 
+ * @date 12 Oct. 2018
+ * 
+ * @author ziyi@outlook.com.au
+ *
+ *****************************************/
+
+public class HandValue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
