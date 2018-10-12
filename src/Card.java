9904a64de8180fa26cc327cafb06515002d5e8ff
ziyi <ziyi.github@outlook.com>
2018-10-12 16:47:01 +1100

    The array of card finished

---
+++ src/Card.java
@@ -0,0 +1,72 @@
+/*****************************************
+ * 
+ * @filename CardEntity.java
+ * 
+ * @project JavaAssessed_Cribbage
+ * 
+ * @brief 	The class of cards
+ * 
+ * @date 	12 Oct. 2018
+ * 
+ * @author 	Ziyi Xiong ziyi@outlook.com.au
+ *
+ *****************************************/
+
+public class Card {
+
+// Attributes
+	private char cardRank; // the rank of card
+	private char cardSuit; // the suit of card
+	private int faceValue;
+	private boolean isStartCard; // the flag of start card
+
+// Constructor
+	Card(String sCard) throws IllegalArgumentException {
+		this.cardRank = sCard.charAt(0);
+		this.cardSuit = sCard.charAt(1);
+		if ("CDHS".indexOf(cardSuit) < 0) {
+			throw new IllegalArgumentException("Invalid Card Suit");
+		}
+		this.faceValue = Character.getNumericValue(cardRank);
+		if (faceValue < 2 || faceValue > 9) {
+			switch (cardRank) {
+				case 'A':
+					this.faceValue = 1;
+					break;
+				case 'T':
+				case 'J':
+				case 'Q':
+				case 'K':
+					this.faceValue = 10;
+					break;
+				default:
+					this.faceValue = 0;
+					throw new IllegalArgumentException("Invalid Card Rank");
+			}
+		}
+		this.isStartCard = false; // need to be updated using getter
+	}
+
+// getter and setter
+	/** the flag of start card */
+	public boolean isStartCard() {
+		return isStartCard;
+	}
+
+	/** set the flag of start card */
+	public void setStartCard(boolean isStartCard) {
+		this.isStartCard = isStartCard;
+	}
+
+	public char getCardRank() {
+		return cardRank;
+	}
+
+	public char getCardSuit() {
+		return cardSuit;
+	}
+
+	public int getFaceValue() {
+		return faceValue;
+	}
+}
--- src/HandValue.java
+++ src/HandValue.java
@@ -1,25 +1,49 @@
+import java.util.ArrayList;
+
 /*****************************************
  * 
  * @filename HandValue.java
  * 
  * @project JavaAssessed_Cribbage
  * 
- * @brief the Java project for COMP90041
+ * @brief 	the Java project for COMP90041
  * 
- * @date 12 Oct. 2018
+ * @date 	12 Oct. 2018
  * 
- * @author ziyi@outlook.com.au
+ * @author 	Ziyi Xiong ziyi@outlook.com.au
+ * 			Peter Schachte schachte@unimelb.edu.au
  *
  *****************************************/
 
 public class HandValue {
 
-	/**
-	 * @param args
-	 */
+// Attributes
+	/** the list of cards */
+	private static Card[] cards;
+	/** all possible combinations of cards */
+	private static Card[][] cardComb;
+	
+// Main Method 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		cards = new Card[5];
+		initCardList(args);
+		cardComb = Combinations.combinations(cards);
 	}
-
+	
+// Methods
+	/** initialize the cards array */
+	private static void initCardList(String[] args) {
+		for (int i = 0; i<5; i++) {
+			try {
+				Card c = new Card(args[i]);
+				cards[i] = c;
+			} catch (IllegalArgumentException e) {
+				System.out.println(e);
+				System.exit(-1);
+			}
+		}
+		cards[4].setStartCard(true);
+	}
+	
+	
 }
